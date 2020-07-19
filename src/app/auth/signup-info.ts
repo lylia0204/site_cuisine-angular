c2cb3819c45a28acd9dc0a801c49a05ddfaaa373
EmiHan84 <hana.mab88@gmail.com>
2020-07-19 04:39:00 +0200

    complement connexion

--- src/app/auth/signup-info.ts
+++ src/app/auth/signup-info.ts
@@ -3,11 +3,14 @@ export class SignUpInfo {
     email: string;
     role: string[];
     password: string;
+   // password2: string;
+                            
 
     constructor( username: string, email: string, password: string) {
         this.username = username;
         this.email = email;
         this.password = password;
+     //   this.password2= password2
         this.role = ['user'];
     }
 }
